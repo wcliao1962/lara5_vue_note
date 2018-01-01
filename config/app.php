24ffea774f9ae694477b68c16f3cc13ba4efb6ea
廖文忠 <wcliao1962@gmail.com>
2018-01-01 17:35:15 +0800

    加入tracy debugger

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Recca0120\LaravelTracy\LaravelTracyServiceProvider::class,
 
     ],
 
